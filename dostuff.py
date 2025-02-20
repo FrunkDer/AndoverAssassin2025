f60949ca4f32d528684daf976b954e44030c653c
Frank Hu <jinfanfrank@gmail.com>
2025-02-20 11:31:28 -0500

    Shuffled and changed task

--- dostuff.py
+++ dostuff.py
@@ -12,10 +12,11 @@ emailAllAssignments()
 kill(first, last, sendemail=Boolean)
 '''
 
-#woof.sendall('''Alright, shuffled everyone and everything is all set! Let me know if you didn't receive an assignment from me. There's 19 people left, and the eliination criteria will change as more people are eliminated!!!''')
+#woof.sendall('''WE ARE DOWN TO OUR TOP 15!
+#Congratulations! We're going to start spicing things up. From now on, instead of taking a picture of your target on their phone, you have to stick a post-it note/sticker onto your target without them noticing, take a picture of them with said post-it note on them, then send it in to @andover.2026. Your targets will also be reshuffled. Stay tuned to receive your target!''')
 
 #woof.shuffle()
 
 #woof.emailAllAssignments()
 
-woof.kill("Sydney", "Jan", sendemail=True)
+#woof.kill("Sydney", "Jan", sendemail=True)
