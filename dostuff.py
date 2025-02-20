97998aac6cb47e336c4871a15bad88b3cafbfa4c
Frank Hu <jinfanfrank@gmail.com>
2025-02-20 18:41:19 -0500

    Killed Sydney and sent Rule Clarification

--- dostuff.py
+++ dostuff.py
@@ -12,11 +12,11 @@ emailAllAssignments()
 kill(first, last, sendemail=Boolean)
 '''
 
-#woof.sendall('''WE ARE DOWN TO OUR TOP 15!
-#Congratulations! We're going to start spicing things up. From now on, instead of taking a picture of your target on their phone, you have to stick a post-it note/sticker onto your target without them noticing, take a picture of them with said post-it note on them, then send it in to @andover.2026. Your targets will also be reshuffled. Stay tuned to receive your target!''')
+#woof.sendall('''A QUICK RULE CLARIFICATION
+#In putting the post-it on your target, your target must NOT BE AWARE OF THE POST IT. If you get a photo of them while they are trying to take it off, IT DOES NOT COUNT''')
 
 #woof.shuffle()
 
 #woof.emailAllAssignments()
 
-#woof.kill("Evie", "Lagrandeur", sendemail=True)
+#woof.kill("Xavier", "Moran", sendemail=True)
