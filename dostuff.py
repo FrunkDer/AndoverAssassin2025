50e58109db9e3b21ce7eef01427a699ea6a8a4e0
Frank Hu <jinfanfrank@gmail.com>
2025-02-28 21:28:24 -0500

    Killed two people

--- dostuff.py
+++ dostuff.py
@@ -12,11 +12,11 @@ emailAllAssignments(pth)
 kill(first, last, sendemail=Boolean, pth)
 '''
 # message = '''
-# Revivals are over!! I'm gonna give you your assignments soon but i have an english paper due soon so gimme like a day'''
+# Clarification: the opening on your container / glass must be equal or larger in area than a commons glass'''
 # woof.sendall(message, "killers.csv")
 
 # woof.shuffle("killers.csv")
 
-#woof.emailAllAssignments("killers.csv")
+# woof.emailAllAssignments("killers.csv")
 
-#woof.kill("Aeva", "Cleare", True, "revivals.csv")
+#woof.kill("Maurice", "Ntoro", True, "killers.csv")
