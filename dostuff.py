dc73c0125343b8dd4e482118588a441c59b962c8
Frank Hu <jinfanfrank@gmail.com>
2025-03-24 12:19:15 -0400

    Shuffled and emailed

--- dostuff.py
+++ dostuff.py
@@ -11,12 +11,15 @@ emailAllAssignments(pth)
 
 kill(first, last, sendemail=Boolean, pth)
 '''
-# message = '''
-# Shuffling assignments!'''
-# woof.sendall(message, "killers.csv")
+message = '''
+Welcome back from break guys!!
+You should all have gotten an email with your target.
+Here's your task: If you're caught walking, you're out. Running, skipping, it's fair game, but if I see a picture of you walking, you're done.
+'''
+woof.sendall(message, "killers.csv")
 
-# woof.shuffle("killers.csv")
+woof.shuffle("killers.csv")
 
-# woof.emailAllAssignments("killers.csv")
+woof.emailAllAssignments("killers.csv")
 
 #woof.kill("Evie", "Lagrandeur", True, "killers.csv")
