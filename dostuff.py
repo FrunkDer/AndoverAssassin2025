9d510ab3d70c3fb6625408f4b686bcb09ce8b7c9
Frank Hu <jinfanfrank@gmail.com>
2025-03-25 10:46:08 -0400

    Aeva Killed Nathan

--- dostuff.py
+++ dostuff.py
@@ -11,15 +11,15 @@ emailAllAssignments(pth)
 
 kill(first, last, sendemail=Boolean, pth)
 '''
-message = '''
-Welcome back from break guys!!
-You should all have gotten an email with your target.
-Here's your task: If you're caught walking, you're out. Running, skipping, it's fair game, but if I see a picture of you walking, you're done.
-'''
-woof.sendall(message, "killers.csv")
+# message = '''
+# Welcome back from break guys!!
+# You should all have gotten an email with your target.
+# Here's your task: If you're caught walking, you're out. Running, skipping, it's fair game, but if I see a picture of you walking, you're done.
+# '''
+# woof.sendall(message, "killers.csv")
 
-woof.shuffle("killers.csv")
+# woof.shuffle("killers.csv")
 
-woof.emailAllAssignments("killers.csv")
+# woof.emailAllAssignments("killers.csv")
 
-#woof.kill("Evie", "Lagrandeur", True, "killers.csv")
+woof.kill("Aeva", "Cleare", sendemail=True, pth="killers.csv")
